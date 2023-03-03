eb8e26cc706db84159e53d6e475b37a7464d3b3b
ateka-p <atekapeter@gmail.com>
2023-03-02 16:18:28 -0800

    Strangeness

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -27,7 +27,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	c1--, c2--, size_r--;
 	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
 	while (bg >= 0)
-	{
+
 	op = dr1 + dr2 + add;
 	if (op >= 10)
 	add = op / 10;
@@ -46,7 +46,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	else
 		dr2 = 0;
 	bg--, size_r--;
-	}
+
 	if (*(r) == '0')
 		return (r + 1);
 	else
