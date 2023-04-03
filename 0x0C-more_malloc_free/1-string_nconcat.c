af568baa73acdfe6940d6345d0b5b532cf1c7934
ateka-p <atekapeter@gmail.com>
2023-04-03 04:20:24 -0700

    String nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,12 +35,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	sout = malloc(lsout + 1);
 
 	if (sout == NULL)
+		return (NULL);
 
-		for (i = 0; i < lsout; i++)
-			if (i < ls1)
-				sout[i] = s1[i];
-			else
-				sout[i] = s2[i - ls1];
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
 
 	sout[i] = '\0';
 
